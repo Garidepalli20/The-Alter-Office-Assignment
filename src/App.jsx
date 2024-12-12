1e0ecee2052f4a5614952cdf22fdb63b5771bc69
Naveena Garidepalli <123829669+Garidepalli20@users.noreply.github.com>
2024-12-12 17:41:26 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import LogIn from "./components/Login/LogIn";
+// import Feeds from "./components/Feeds/Feeds";
+
+const App = () => {
+  return (
+    <div>
+      {/* <LogIn /> */}
+      {/* <Feeds /> */}
+      
+    </div>
+  );
+};
+
+export default App;
