3d70fe2ec06ba26481c2f1e215722a30e4a1fe2e
Naveena Garidepalli <123829669+Garidepalli20@users.noreply.github.com>
2024-12-12 17:30:37 +0530

    Create ImageContainer.jsx

---
+++ src/components/Login/ImageContainer.jsx
@@ -0,0 +1,83 @@
+const ImageContainer = () => {
+  return (
+    <>
+      <div>
+        <div id="first-part" className="grid grid-cols-3 relative">
+          <div>
+            <img
+              src="/assets/Img/Signin/Image1.png"
+              alt="image1"
+              className="w-[125px] h-[207px] absolute left-[-10px] top-[-23px]"
+            />
+          </div>
+
+          <div>
+            <img
+              src="/assets/Img/Signin/Image2.png"
+              alt="image2"
+              className="w-[125px] h-[207px] absolute top-[-142px] left-[124px]"
+            />
+          </div>
+
+          <div>
+            <img
+              src="/assets/Img/Signin/Image3.png"
+              alt="image3"
+              className="w-[125px] h-[207px] absolute left-[256px] top-[-23px]"
+            />
+          </div>
+
+          <div>
+            <img
+              src="/assets/Img/Signin/Image4.png"
+              alt="image4"
+              className="w-[125px] h-[207px] absolute left-[-9px] top-[192px]"
+            />
+          </div>
+
+          <div>
+            <img
+              src="/assets/Img/Signin/Image5.png"
+              alt="image5"
+              className="w-[125px] h-[207px] absolute"
+            />
+          </div>
+
+          <div>
+            <img
+              src="/assets/Img/Signin/Image6.png"
+              alt="image6"
+              className="w-[125px] h-[207px] absolute"
+            />
+          </div>
+
+          <div>
+            <img
+              src="/assets/Img/Signin/Image7.png"
+              alt="image7"
+              className="w-[125px] h-[207px] absolute"
+            />
+          </div>
+
+          <div>
+            <img
+              src="/assets/Img/Signin/Image8.png"
+              alt="image8"
+              className="w-[125px] h-[207px] absolute"
+            />
+          </div>
+
+          <div>
+            <img
+              src="/assets/Img/Signin/Image9.png"
+              alt="image9"
+              className="w-[125px] h-[207px] absolute"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ImageContainer;
